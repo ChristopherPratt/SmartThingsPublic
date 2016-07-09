514d5fcaf7531c919947b818c1a998ef56427f94
christopher r pratt <chrisprattmt@gmail.com>
2016-07-08 21:03:50 -0400

    using alot of multiChannelAssociationSet() messages instead of the normal association messages because according to the SDS document: association messages by themselves don't play nice with multi-channel applications

--- devicetypes/fortrezz/mimo2.src/mimo2.groovy
+++ devicetypes/fortrezz/mimo2.src/mimo2.groovy
@@ -44,10 +44,7 @@ metadata {
     
         input ("RelaySwitchDelay2", "decimal", title: "Relay 2 Delay between relay switch on and off in seconds. Only Numbers 0 to 3 allowed. 0 value will remove delay and allow relay to function as a standard switch. Press 'Configure' tile to allow change", description: "Numbers 0 to 3 allowed.", defaultValue: 0, required: false, displayDuringSetup: true)
         } // the range would be 0 to 3.1, but the range value would not accept 3.1, only whole numbers (i tried paranthesis and fractions too. :( )
-        input ("SIG1AD", "bool", title: "Choose setting of SIG1 Behavior: Swipe right for analogue and swipe left for digital.", defaultValue: false, required: false, displayDuringSetup: true)
-        input ("SIG2AD", "bool", title: "Choose setting of SIG2 Behavior: Swipe right for analogue and swipe left for digital.", defaultValue: false, required: false, displayDuringSetup: true)
-
-    
+       
 	tiles {
          standardTile("switch", "device.switch", width: 2, height: 2) {
             state "on", label: "Relay 1 On", action: "off", icon: "http://swiftlet.technology/wp-content/uploads/2016/06/Switch-On-104-edit.png", backgroundColor: "#53a7c0"            
@@ -110,13 +107,10 @@ def updated() { // neat built-in smartThings function which automatically runs w
     return response(configure()) // the response() function is used for sending commands in reponse to an event, without it, no zWave commands will work for contained function
 }
 
-def zwaveEvent(int endPoint, physicalgraph.zwave.commands.basicv1.BasicSet cmd) // basic set is essentially our digital sensor for SIG1 and SIG2
+def zwaveEvent(physicalgraph.zwave.commands.basicv1.BasicSet cmd) // basic set is essentially our digital sensor for SIG1 and SIG2 - it doesn't use an endpoint so we are having it send a multilevelGet() for SIG1 and SIG2 to see which one triggered.
 {
 	log.debug "sent a BasicSet command"
-	if (endPoint == 1)
-    	{return [name: "contact", value: cmd.value ? "open" : "closed"]}
-    else if (endPoint == 2) 
-    	{return [name: "contact2", value: cmd.value ? "open" : "closed"]}
+	return response(refresh())
 }
 
 def zwaveEvent(int endPoint, physicalgraph.zwave.commands.sensorbinaryv1.SensorBinaryReport cmd) // event to get the state of the digital sensor SIG1 and SIG2
@@ -157,18 +151,25 @@ def zwaveEvent(int endPoint, physicalgraph.zwave.commands.switchbinaryv1.SwitchB
 def zwaveEvent (int endPoint, physicalgraph.zwave.commands.sensormultilevelv5.SensorMultilevelReport cmd) // sensorMultilevelReport is used to report the value of the analog voltage for SIG1
 {
 	def map = [:]
-    def volatageVal = CalculateVoltage(cmd.scaledSensorValue) // saving the scaled Sensor Value used to enter into a large formula to determine actual voltage value
+    def voltageVal = CalculateVoltage(cmd.scaledSensorValue) // saving the scaled Sensor Value used to enter into a large formula to determine actual voltage value
     if (endPoint == 1)
     {
         map.name = "voltage"
-        map.value = volatageVal
+        map.value = voltageVal
         map.unit = "v"
+        if (voltageVal < 1) {
+        sendEvent(name: "contact", value: "closed")
+        log.debug "$voltageVal"
+        }
+        else {sendEvent(name: "contact", value: "open")}
     }
     else if (endPoint == 2)
     {
         map.name = "voltage2"
-        map.value = volatageVal
+        map.value = voltageVal
         map.unit = "v"
+        if (voltageVal < 1) {sendEvent(name: "contact2", value: "closed")}
+        else {sendEvent(name: "contact2", value: "open")}
     }
 	log.debug "sent a SensorMultilevelReport $map.name"
     return map
@@ -192,6 +193,13 @@ def zwaveEvent(physicalgraph.zwave.commands.multichannelv3.MultiChannelCmdEncap 
     if (encapsulatedCommand) {
         return zwaveEvent(cmd.sourceEndPoint, encapsulatedCommand)
     }
+}
+
+def zwaveEvent(int endPoint, physicalgraph.zwave.commands.multichannelassociationv2.MultiChannelAssociationReport cmd) {
+	
+    log.debug "sent an Association Report"
+    log.debug " ${cmd.groupingIdentifier}"
+    //return [:]
 }
 
 def zwaveEvent(physicalgraph.zwave.Command cmd) {
@@ -231,22 +239,31 @@ def configure() {
     	log.debug "Relay 2 input ${delay2 / 10} set too low. Min value is 0"
     	delay = 0
     } 
-    def SIG1ADsetting
-    if (SIG1AD) {SIG1ADsetting = 0x40}
-    else { SIG1ADsetting = 0x01}
-    def SIG2ADsetting
-    if (SIG2AD) {SIG2ADsetting = 0x40}
-    else { SIG2ADsetting = 0x01}
+
     
     
     return delayBetween([
-    	encap(zwave.associationV1.associationSet(groupingIdentifier:1, nodeId:[zwaveHubNodeId]), 0), // sending the 5 associationSet messages forces the MIMO2 to be in default configuration
-    	encap(zwave.associationV1.associationSet(groupingIdentifier:1, nodeId:[zwaveHubNodeId]), 1),
-    	encap(zwave.associationV1.associationSet(groupingIdentifier:1, nodeId:[zwaveHubNodeId]), 2),
-        encap(zwave.associationV1.associationSet(groupingIdentifier:1, nodeId:[zwaveHubNodeId]), 3),
-        encap(zwave.associationV1.associationSet(groupingIdentifier:1, nodeId:[zwaveHubNodeId]), 4),
-        secure(zwave.configurationV1.configurationSet(configurationValue: [SIG1ADsetting], parameterNumber: 3, size: 1)), // sends a multiLevelSensor report every 30 seconds for SIG1
-        secure(zwave.configurationV1.configurationSet(configurationValue: [SIG2ADsetting], parameterNumber: 9, size: 1)), // sends a multiLevelSensor report every 30 seconds for SIG2
+        encap(zwave.multiChannelAssociationV2.multiChannelAssociationSet(groupingIdentifier:3, nodeId:[zwaveHubNodeId]), 0),
+        encap(zwave.multiChannelAssociationV2.multiChannelAssociationSet(groupingIdentifier:2, nodeId:[zwaveHubNodeId]), 0),
+
+        encap(zwave.multiChannelAssociationV2.multiChannelAssociationSet(groupingIdentifier:2, nodeId:[zwaveHubNodeId]), 1),
+        encap(zwave.multiChannelAssociationV2.multiChannelAssociationSet(groupingIdentifier:2, nodeId:[zwaveHubNodeId]), 2),
+        encap(zwave.multiChannelAssociationV2.multiChannelAssociationSet(groupingIdentifier:1, nodeId:[zwaveHubNodeId]), 3),
+        encap(zwave.multiChannelAssociationV2.multiChannelAssociationSet(groupingIdentifier:1, nodeId:[zwaveHubNodeId]), 4),
+        /*encap(zwave.multiChannelAssociationV2.multiChannelAssociationRemove(groupingIdentifier:1, nodeId:[zwaveHubNodeId]), 0), // sending the 5 associationSet messages forces the MIMO2 to be in default configuration
+    	encap(zwave.multiChannelAssociationV2.multiChannelAssociationRemove(groupingIdentifier:1, nodeId:[zwaveHubNodeId]), 1),
+    	encap(zwave.multiChannelAssociationV2.multiChannelAssociationRemove(groupingIdentifier:1, nodeId:[zwaveHubNodeId]), 2),*/
+
+    	
+
+		/*encap(zwave.associationV2.associationSet(groupingIdentifier:2, nodeId:[zwaveHubNodeId]), 0), // sending the 5 associationSet messages forces the MIMO2 to be in default configuration
+        encap(zwave.associationV2.associationSet(groupingIdentifier:3, nodeId:[zwaveHubNodeId]), 0), // sending the 5 associationSet messages forces the MIMO2 to be in default configuration
+    	encap(zwave.associationV2.associationSet(groupingIdentifier:2, nodeId:[zwaveHubNodeId]), 1),
+    	encap(zwave.associationV2.associationSet(groupingIdentifier:2, nodeId:[zwaveHubNodeId]), 2),
+        encap(zwave.associationV2.associationSet(groupingIdentifier:1, nodeId:[zwaveHubNodeId]), 3),
+        encap(zwave.associationV2.associationSet(groupingIdentifier:1, nodeId:[zwaveHubNodeId]), 4),*/
+        secure(zwave.configurationV1.configurationSet(configurationValue: [0x40], parameterNumber: 3, size: 1)), // sends a multiLevelSensor report every 30 seconds for SIG1
+        secure(zwave.configurationV1.configurationSet(configurationValue: [0x40], parameterNumber: 9, size: 1)), // sends a multiLevelSensor report every 30 seconds for SIG2
         secure(zwave.configurationV1.configurationSet(configurationValue: [delay], parameterNumber: 1, size: 1)), // configurationValue for parameterNumber means how many 100ms do you want the relay
         																										// to wait before it cycles again / size should just be 1 (for 1 byte.)
         secure(zwave.configurationV1.configurationSet(configurationValue: [delay2], parameterNumber: 2, size: 1)),
@@ -288,8 +305,12 @@ def refresh() {
          encap(zwave.sensorMultilevelV5.sensorMultilevelGet(), 1),// requests a report of the anologue input voltage for SIG1
          encap(zwave.sensorMultilevelV5.sensorMultilevelGet(), 2),// requests a report of the anologue input voltage for SIG2
          encap(zwave.switchBinaryV1.switchBinaryGet(), 3), //requests a report of the relay to make sure that it changed for Relay 1
-         encap(zwave.switchBinaryV1.switchBinaryGet(), 4) //requests a report of the relay to make sure that it changed for Relay 2
-       ],200)
+         encap(zwave.switchBinaryV1.switchBinaryGet(), 4), //requests a report of the relay to make sure that it changed for Relay 2
+         encap(zwave.multiChannelAssociationV2.multiChannelAssociationGet(groupingIdentifier: 1), 0), 
+         encap(zwave.multiChannelAssociationV2.multiChannelAssociationGet(groupingIdentifier: 1), 1), 
+    	 encap(zwave.multiChannelAssociationV2.multiChannelAssociationGet(groupingIdentifier: 1), 2)
+
+       ],400)
 }
 
 private secureSequence(commands, delay=200) { // decided not to use this
